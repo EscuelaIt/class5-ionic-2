dbfd0807a4789c843df4e22cbc1c213708e77e30
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-07-21 13:09:01 -0500

    save

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Alert, Loading } from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/page1/page1.html'
@@ -8,6 +8,53 @@ export class Page1 {
 
   time;
 
-  constructor(private navController: NavController) {
+  constructor(
+    private navCtrl: NavController
+  ) {
+  }
+
+  showLoad(){
+    let load = Loading.create({
+      content: 'Cargando'
+    });
+    this.navCtrl.present( load );
+    setTimeout(()=>{
+      load.dismiss();
+    }, 5000);
+  }
+
+  showAlert(): void{
+    let alert = Alert.create({
+      title: 'Hola',
+      subTitle: 'Hola',
+      buttons: ['Aceptar']
+    });
+    this.navCtrl.present( alert );
+  }
+
+  showConfirm(): void{
+    let confirm = Alert.create({
+      title: 'realmente de eliminar',
+      message: 'lorem ksjdhfksdf skjdfhsdf hjdsfkdsf jkdfh',
+      buttons: [
+        {
+          text: 'Aceptar',
+          handler: () =>{
+            this.someRest();
+          }
+        },
+        {
+          text: 'Cancel',
+          handler: () =>{
+            console.log('cancel');
+          }
+        }
+      ]
+    });
+    this.navCtrl.present( confirm );
+  }
+
+  someRest(){
+
   }
 }
